1e2ae860b10fcbdb74bbf6d8d70d7fa7a6212926
lebmouse <gtah2yk@gmail.com>
2019-07-30 13:56:27 +0900

    코멘트 수정, 종하요 모델 추가

--- review/models.py
+++ review/models.py
@@ -18,11 +18,27 @@ class Review(models.Model):
     def __str__(self):
         return self.title
 
-class Comments(models.Model):
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Review, related_name="comments", null=True, on_delete=models.CASCADE)
     body = models.CharField(null=True)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.created_by.name
+
+
 class Image(models.Model):
     review = models.ForeignKey(Review, null=True, on_delete=models.CASCADE)
     image = models.ImageField(
         upload_to='images/', null=True, max_length=None, blank=True)
+
+
+class Like(models.Model):
+    reivews = models.ForeignKey(
+        Review, related_name="likes", null=True, on_delete=models.CASCADE)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
