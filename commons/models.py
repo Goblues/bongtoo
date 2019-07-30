bf94f3923c30ddf2a9bfb86319380a40df750480
lebmouse <gtah2yk@gmail.com>
2019-07-30 23:22:58 +0900

    commons model __str__ 고침

--- commons/models.py
+++ commons/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Region(models.Model):
     city = models.CharField(null=True, blank=True, max_length=100)
     town = models.CharField(null=True, blank=True, max_length=100)
 
     def __str__(self):
-        return self.name
+        return '{},{}'.format(self.city, self.town)
 
 
 class Activity(models.Model):
@@ -15,5 +17,9 @@ class Activity(models.Model):
     def __str__(self):
         return self.name
 
+
 class Subject(models.Model):
-    name = models.CharField(null=True,blank=True, max_length=100)
+    name = models.CharField(null=True, blank=True, max_length=100)
+
+    def __str__(self):
+        return self.name
