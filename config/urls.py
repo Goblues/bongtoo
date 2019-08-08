2ac221700411b891a8f9a3a7349823b9ee315c27
peterpark1011 <petepp@naver.com>
2019-08-08 12:00:09 +0900

    api 문서 만듬

--- config/urls.py
+++ config/urls.py
@@ -7,20 +7,38 @@ from django.conf import settings
 from django.urls import path, include, re_path
 from django.contrib.auth.decorators import login_required
 from django.contrib import admin
+from django.conf.urls import url
 # admin.autodiscover()
 
 # rest_framework
 from allauth.account.views import confirm_email
-# app's view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+from drf_yasg.views import get_schema_view
+from django.urls import include
+# app_name = "docs"
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Bongtoo API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="petepp@naver.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
     # root
     path('', ApiRoot.as_view(), name="api"),
-    # auth
     path('rest-auth/', include('rest_auth.urls')),
+    # auth
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
     re_path(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
     # user
@@ -28,4 +46,8 @@ urlpatterns = [
     # review
     path('api/search/reviews/', SearchReviewList.as_view(), name="search_reviews"),
     path('reviews/',include('review.urls')),
+    # path('v1/', include('docs.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
